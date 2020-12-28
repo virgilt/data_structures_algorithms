f16ef3eac367c23f830ea0db8513901a99ee23ad
virgilt <travisjvirgil@gmail.com>
2020-12-27 23:11:38 -0500

    Output the integer value of the reverse bits of the input.

--- ReverseBits.java
+++ ReverseBits.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 class ReverseBits {
 
     static int reverseBits(int input) {
-        return input;
+        int output = 0;
+        while (input != 0) {
+            output = output << 1;
+
+            if ((input & 1) == 1) {
+                output |= 1;
+            }
+
+            input = input >> 1;
+        }
+
+        return output;
     }
 
     public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(System.in);
 
+        System.out.print("Enter an integer: ");
+        int num = keyboardInput.nextInt();
+
+        System.out.println(reverseBits(num));
     }
 }
